b5f3c770313d6787c4a182a498979db694857632
shusuke919 <sh091965@gmail.com>
2022-09-19 12:07:07 +0900

    ナビゲーションfix 26:59

--- glx-trvl/src/components/Navbar.js
+++ glx-trvl/src/components/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './NavbarStyles.css'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const[click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+
+
+
   return (
     <div className='header'>
       <Link to='/'><h1>GLX TRVL</h1></Link>
-      <ul className='nav-menu'>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
         <li>
           <Link to='/'>Home</Link>
         </li>
@@ -21,8 +26,9 @@ const Navbar = () => {
           <Link to='/contact'>Contact</Link>
         </li>
       </ul>
-      <div className='hamburger'>
-        <FaBars size={20} style={{color: '#fff'}}/>
+      <div className='hamburger' onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{color: '#fff'}} />) : (<FaBars size={20} style={{color: '#fff'}}/>)}
+        
       </div>
     </div>
   )
