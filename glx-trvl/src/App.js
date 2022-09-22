d6bda129b6d38d9d38224917d445d21ee06345d5
shusuke919 <sh091965@gmail.com>
2022-09-22 18:27:18 +0900

    fix

--- glx-trvl/src/App.js
+++ glx-trvl/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Contact from "./routes/Contact";
 
 import Home from "./routes/Home";
 import Pricing from "./routes/Pricing";
@@ -12,6 +13,7 @@ function App() {
      <Route path='/' element={<Home />} />
      <Route path='/pricing' element={<Pricing />} />
      <Route path='/training' element={<Training />} />
+     <Route path='/contact' element={<Contact />} />
     </Routes>
     </>
   );
